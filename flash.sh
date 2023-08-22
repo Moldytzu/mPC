d141183f86f488cdd24007def0c833b3062a2c84
Moldytzu <m.alex123@yahoo.com>
2023-08-22 14:35:22 +0300

    display data and rw

--- flash.sh
+++ flash.sh
@@ -1,3 +1,3 @@
 #cp build/monitor.uf2 /media/$USER/RPI-RP2
-sudo picotool load -f build/monitor.uf2
-sudo picotool reboot -f
+picotool load -f build/monitor.uf2
+picotool reboot -f
--- pico/monitor.c
+++ pico/monitor.c
@@ -1,29 +1,50 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-const uint8_t clockPin = 16;
+// cpu pins
+#define ADDRESS_START 0
+#define ADDRESS_END 15
+#define DATA_START 17
+#define DATA_END 28
+#define CLOCK 16
+#define RW 28
+
 bool clockState = false;
 
 void printState()
 {
     uint16_t address = 0;
+    uint8_t data = 0;
+    bool read = gpio_get(RW);
 
-    for (int i = 0; i <= 15; i++)
+    for (int i = ADDRESS_START; i <= ADDRESS_END; i++)
     {
         bool state = gpio_get(i);
-        address >>= 1;
-        address |= state << 15;
+        // address >>= 1;
+        // address |= state << 15;
+        address <<= 1;
+        address |= state;
     }
 
-    printf("address: %x\n", address);
+    for (int i = DATA_START; i <= DATA_END; i++)
+    {
+        bool state = gpio_get(i);
+        // address >>= 1;
+        // address |= state << 7;
+        data <<= 1;
+        data |= state;
+    }
+
+    printf("%s address: %x data: %x\n", read ? "r" : "w", address, data);
 }
 
 bool clockHandler(struct repeating_timer *t)
 {
     clockState = !clockState;
-    gpio_put(clockPin, clockState);
+    gpio_put(CLOCK, clockState);
 
-    printState();
+    if (!clockState) // print state on falling edge
+        printState();
 
     return true;
 }
@@ -32,19 +53,26 @@ int main()
 {
     stdio_usb_init();
 
-    set_sys_clock_khz(133000, true);
+    gpio_init(CLOCK);
+    gpio_set_dir(CLOCK, GPIO_OUT);
 
-    gpio_init(clockPin);
-    gpio_set_dir(clockPin, GPIO_OUT);
+    gpio_init(RW);
+    gpio_set_dir(RW, GPIO_IN);
 
-    for (int i = 0; i <= 15; i++)
+    for (int i = ADDRESS_START; i <= ADDRESS_END; i++)
+    {
+        gpio_init(i);
+        gpio_set_dir(i, GPIO_IN);
+    }
+
+    for (int i = DATA_START; i <= DATA_END; i++)
     {
         gpio_init(i);
         gpio_set_dir(i, GPIO_IN);
     }
 
     struct repeating_timer timer;
-    add_repeating_timer_us(10 /*100 khz*/, clockHandler, NULL, &timer);
+    add_repeating_timer_us(-8 /*125 khz*/, clockHandler, NULL, &timer);
 
     while (1)
         ;
