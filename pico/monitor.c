8c1dcac90d02c4a5e2a0b8df72cb7f944c83dd19
Moldytzu <m.alex123@yahoo.com>
2023-08-22 12:57:53 +0300

    monitor prototype

--- pico/monitor.c
+++ pico/monitor.c
@@ -1,19 +1,51 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+const uint8_t clockPin = 16;
+bool clockState = false;
+
+void printState()
+{
+    uint16_t address = 0;
+
+    for (int i = 0; i <= 15; i++)
+    {
+        bool state = gpio_get(i);
+        address >>= 1;
+        address |= state << 15;
+    }
+
+    printf("address: %x\n", address);
+}
+
+bool clockHandler(struct repeating_timer *t)
+{
+    clockState = !clockState;
+    gpio_put(clockPin, clockState);
+
+    printState();
+
+    return true;
+}
+
 int main()
 {
-    stdio_init_all();
+    stdio_usb_init();
 
-    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-    while (true)
+    set_sys_clock_khz(133000, true);
+
+    gpio_init(clockPin);
+    gpio_set_dir(clockPin, GPIO_OUT);
+
+    for (int i = 0; i <= 15; i++)
     {
-        printf("serbus");
-        gpio_put(LED_PIN, 1);
-        sleep_ms(250);
-        gpio_put(LED_PIN, 0);
-        sleep_ms(250);
+        gpio_init(i);
+        gpio_set_dir(i, GPIO_IN);
     }
+
+    struct repeating_timer timer;
+    add_repeating_timer_us(10 /*100 khz*/, clockHandler, NULL, &timer);
+
+    while (1)
+        ;
 }
